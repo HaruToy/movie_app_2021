0e5e069e86280d590dceaeedfe0f5950cfd04fad
HaruToy <twitty3420@gmail.com>
2021-07-21 00:05:33 +0900

    03 컴포넌트와 jsx and props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+function Food(potato){
+  //console.log(potato);
+  return <h1>I like {potato.fav}</h1>;
+}
+//컴포넌트로 데이터를 보내는 방법 : props
+//단, 문자열을 제외한 경우는 모두 중괄호로 값을 감싸야 한다
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div>
+      <h2>Hello!!!</h2> 
+      
+      <Food fav="kimchi"/>
+      <Food fav="ramen"/>
+      <Food fav="쭈꾸미"/>
+      <Food fav="고르곤졸라 피자"/>
+      <Food fav="명란 파스타"/>
+  </div>);
 }
 
 export default App;
